c8394ad6f9e46c09a8fda3f49d18b20ecd25df99
Ushira Dineth <ushiradineth@gmail.com>
2023-09-25 01:09:18 +0530

    fix(dashboard): fixed redirects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-function index() {
-  return <div>index</div>;
-}
+import { getSession } from "next-auth/react";
+import { type GetServerSideProps } from "next";
 
-export default index;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ ctx: context });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+      props: {},
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
+export default function Dashboard() {
+  return <div>Excel like Dashboard</div>;
+}

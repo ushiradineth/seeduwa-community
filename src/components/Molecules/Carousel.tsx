b099b2a1b59700ae4b5f20ca64b9fc2e327bd970
Ushira Dineth <ushiradineth@gmail.com>
2023-09-25 00:52:03 +0530

    fix(ci): fixed build error

--- src/components/Molecules/Carousel.tsx
+++ src/components/Molecules/Carousel.tsx
@@ -11,7 +11,7 @@ type Props = {
   activeIndex?: number;
 };
 
-export default function Carousel({ children, navButtons, activeIndex: activeIndexProp = 0, indicators }: Props) {
+export default function Carousel({ children, activeIndex: activeIndexProp = 0, indicators }: Props) {
   const gridRef = useRef<HTMLDivElement | null>(null);
   const [width, setWidth] = useState(0);
   const { width: windowWidth } = useWindowDimensions();
@@ -21,37 +21,37 @@ export default function Carousel({ children, navButtons, activeIndex: activeInde
     gridRef.current && gridRef.current.children[0] instanceof HTMLElement && setWidth(gridRef.current.children[0].offsetWidth);
   }, [windowWidth]);
 
-  const handleScrollRight = useCallback(() => {
-    if (gridRef.current) {
-      if (gridRef.current.scrollLeft + 48 + width > gridRef.current.scrollWidth) {
-        gridRef.current.scrollTo({
-          left: 0,
-          behavior: "smooth",
-        });
-      } else {
-        gridRef.current.scrollTo({
-          left: gridRef.current.scrollLeft + width,
-          behavior: "smooth",
-        });
-      }
-    }
-  }, [width]);
+  // const handleScrollRight = useCallback(() => {
+  //   if (gridRef.current) {
+  //     if (gridRef.current.scrollLeft + 48 + width > gridRef.current.scrollWidth) {
+  //       gridRef.current.scrollTo({
+  //         left: 0,
+  //         behavior: "smooth",
+  //       });
+  //     } else {
+  //       gridRef.current.scrollTo({
+  //         left: gridRef.current.scrollLeft + width,
+  //         behavior: "smooth",
+  //       });
+  //     }
+  //   }
+  // }, [width]);
 
-  const handleScrollLeft = useCallback(() => {
-    if (gridRef.current) {
-      if (gridRef.current.scrollLeft === 0) {
-        gridRef.current.scrollTo({
-          left: gridRef.current.scrollWidth,
-          behavior: "smooth",
-        });
-      } else {
-        gridRef.current.scrollTo({
-          left: gridRef.current.scrollLeft - width,
-          behavior: "smooth",
-        });
-      }
-    }
-  }, [width]);
+  // const handleScrollLeft = useCallback(() => {
+  //   if (gridRef.current) {
+  //     if (gridRef.current.scrollLeft === 0) {
+  //       gridRef.current.scrollTo({
+  //         left: gridRef.current.scrollWidth,
+  //         behavior: "smooth",
+  //       });
+  //     } else {
+  //       gridRef.current.scrollTo({
+  //         left: gridRef.current.scrollLeft - width,
+  //         behavior: "smooth",
+  //       });
+  //     }
+  //   }
+  // }, [width]);
 
   const scrollTo = useCallback(
     (to: number, behavior: ScrollBehavior = "smooth") => {
@@ -88,36 +88,36 @@ export default function Carousel({ children, navButtons, activeIndex: activeInde
     );
   }, [activeIndex, children, scrollTo]);
 
-  const NavButtons = useCallback(() => {
-    return (
-      <>
-        <button
-          type="button"
-          className="eft-0 group absolute top-0 z-30 hidden h-full cursor-default items-center justify-center px-4 opacity-50 focus:outline-none group-hover:flex">
-          <span
-            onClick={handleScrollLeft}
-            className="inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gray-300/30 group-hover:bg-gray-300/60 group-focus:outline-none group-focus:ring-4 group-focus:ring-gray-300/70">
-            <svg className="h-4 w-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="group absolute right-0 top-0 z-30 hidden h-full cursor-default items-center justify-center px-4 opacity-50 focus:outline-none group-hover:flex">
-          <span
-            onClick={handleScrollRight}
-            className="inline-flex h-10 w-10 cursor-pointer  items-center justify-center rounded-full bg-gray-300/10 group-hover:bg-gray-300/60 group-focus:outline-none group-focus:ring-4 group-focus:ring-gray-300/70">
-            <svg className="h-4 w-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
-      </>
-    );
-  }, [handleScrollLeft, handleScrollRight]);
+  // const NavButtons = useCallback(() => {
+  //   return (
+  //     <>
+  //       <button
+  //         type="button"
+  //         className="eft-0 group absolute top-0 z-30 hidden h-full cursor-default items-center justify-center px-4 opacity-50 focus:outline-none group-hover:flex">
+  //         <span
+  //           onClick={handleScrollLeft}
+  //           className="inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gray-300/30 group-hover:bg-gray-300/60 group-focus:outline-none group-focus:ring-4 group-focus:ring-gray-300/70">
+  //           <svg className="h-4 w-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+  //             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+  //           </svg>
+  //           <span className="sr-only">Previous</span>
+  //         </span>
+  //       </button>
+  //       <button
+  //         type="button"
+  //         className="group absolute right-0 top-0 z-30 hidden h-full cursor-default items-center justify-center px-4 opacity-50 focus:outline-none group-hover:flex">
+  //         <span
+  //           onClick={handleScrollRight}
+  //           className="inline-flex h-10 w-10 cursor-pointer  items-center justify-center rounded-full bg-gray-300/10 group-hover:bg-gray-300/60 group-focus:outline-none group-focus:ring-4 group-focus:ring-gray-300/70">
+  //           <svg className="h-4 w-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+  //             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+  //           </svg>
+  //           <span className="sr-only">Next</span>
+  //         </span>
+  //       </button>
+  //     </>
+  //   );
+  // }, [handleScrollLeft, handleScrollRight]);
 
   return (
     <div className={`no-scrollbar group relative flex overflow-scroll`}>

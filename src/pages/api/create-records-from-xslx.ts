82a848d0abc2018a3dfe1550ea8a1539b8bd39aa
Ushira Dineth <ushiradineth@gmail.com>
2023-11-21 08:47:12 +0530

    fix: scripts

--- src/pages/api/create-records-from-xslx.ts
+++ src/pages/api/create-records-from-xslx.ts
@@ -1,79 +1,88 @@
 // import fs from "fs";
+// import moment from "moment";
 // import * as XLSX from "xlsx/xlsx.mjs";
-import { type NextApiRequest, type NextApiResponse } from "next";
+// import { type NextApiRequest, type NextApiResponse } from "next";
 
 // import { prisma } from "@/server/db";
 
-// const excelFilePath = "./data.xlsx";
+// const excelFilePath = "./a.xlsx";
 
-export default function handler(request: NextApiRequest, response: NextApiResponse) {
-  // const db = prisma;
-  // XLSX.set_fs(fs);
-  // const workbook = XLSX.readFile(excelFilePath);
-  // const sheetName = workbook.SheetNames[0];
-  // const worksheet = workbook.Sheets[sheetName];
-  // const months = ["March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// export default function handler(request: NextApiRequest, response: NextApiResponse) {
+//   const db = prisma;
+//   XLSX.set_fs(fs);
+//   const workbook = XLSX.readFile(excelFilePath);
+//   const sheetName = workbook.SheetNames[0];
+//   const worksheet = workbook.Sheets[sheetName];
+//   const months = ["March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-  // const members = [];
+//   const members = [];
 
-  // for (let rowIndex = 1; ; rowIndex++) {
-  //   const row = XLSX.utils.encode_row(rowIndex);
-  //   const houseid = worksheet[`A${row}`] ? worksheet[`A${row}`].v : null;
-  //   const name = worksheet[`B${row}`] ? worksheet[`B${row}`].v : null;
+//   for (let rowIndex = 2; ; rowIndex++) {
+//     const row = XLSX.utils.encode_row(rowIndex);
+//     const lane = worksheet[`A${row}`] ? String(worksheet[`A${row}`].v).trim() : null;
+//     const houseid = worksheet[`B${row}`] ? String(worksheet[`B${row}`].v).trim() : null;
+//     const name = worksheet[`C${row}`] ? String(worksheet[`C${row}`].v).trim() : null;
+//     const number = worksheet[`D${row}`] ? String(worksheet[`D${row}`].v).trim() : undefined;
 
-  //   if (!houseid && !name) {
-  //     break;
-  //   }
+//     console.log(houseid);
 
-  //   const rowData: {
-  //     memberId: string;
-  //     houseid: string;
-  //     name: string;
-  //     payments: { month: string; amount: number }[];
-  //   } = {
-  //     memberId: "",
-  //     houseid,
-  //     name,
-  //     payments: [],
-  //   };
+//     if (!houseid && !name && !lane) {
+//       break;
+//     }
 
-  //   // Add monthly data
-  //   for (let i = 0; i < months.length; i++) {
-  //     const monthHeader = worksheet[XLSX.utils.encode_col(i + 2) + "1"] ? worksheet[XLSX.utils.encode_col(i + 2) + "1"].v : null;
-  //     const monthValue = worksheet[XLSX.utils.encode_col(i + 2) + row] ? worksheet[XLSX.utils.encode_col(i + 2) + row].v : null;
+//     const rowData: {
+//       memberId: string;
+//       lane: string | null;
+//       houseid: string | null;
+//       name: string | null;
+//       number: string | undefined;
+//       payments: { month: string; amount: number }[];
+//     } = {
+//       memberId: "",
+//       lane,
+//       houseid,
+//       name,
+//       number: number ? (number?.length === 9 ? "+94" + number : !number?.startsWith("+") ? "+" + number : number) : undefined,
+//       payments: [],
+//     };
 
-  //     if (monthHeader && monthValue) {
-  //       rowData.payments.push({ month: monthHeader, amount: monthValue });
-  //     }
-  //   }
+//     // Add monthly data
+//     for (let i = 0; i < months.length; i++) {
+//       const monthHeader = worksheet[XLSX.utils.encode_col(i + 4) + "1"] ? worksheet[XLSX.utils.encode_col(i + 4) + "1"].v : null;
+//       const monthValue = worksheet[XLSX.utils.encode_col(i + 4) + row] ? worksheet[XLSX.utils.encode_col(i + 4) + row].v : null;
 
-  //   members.push(rowData);
-  // }
+//       if (monthHeader && monthValue) {
+//         rowData.payments.push({ month: monthHeader, amount: monthValue });
+//       }
+//     }
 
-  // members.forEach(async (member) => {
-  //   const memberData = await db.member.findFirst({ where: { houseId: String(member.houseid) } });
+//     members.push(rowData);
+//   }
 
-  //   member.payments.forEach(async (payment) => {
-  //     await db.payment.create({
-  //       data: {
-  //         member: {
-  //           connect: {
-  //             id: memberData?.id,
-  //           },
-  //         },
-  //         amount: payment.amount,
-  //         month: month={moment(
-          //     moment()
-          //     .year(year)
-          //     .month(MONTHS.findIndex((value) => value === month))
-          //     .startOf("month")
-          //     .utcOffset(0, true)
-          //     .format(),
-          // ).toDate()}
-  //       },
-  //     });
-  //   });
-  // });
+//   members.forEach(async (member) => {
+//     const memberData = await db.member.create({
+//       data: {
+//         houseId: member.houseid!,
+//         lane: member.lane!,
+//         name: member.name!,
+//         phoneNumber: member.number ?? undefined,
+//       },
+//     });
 
-  response.status(200).json({});
-}
+//     member.payments.forEach(async (payment) => {
+//       await db.payment.create({
+//         data: {
+//           member: {
+//             connect: {
+//               id: memberData?.id,
+//             },
+//           },
+//           amount: payment.amount,
+//           month: moment().year(2023).month(payment.month).startOf("month").utcOffset(0, true).toDate(),
+//         },
+//       });
+//     });
+//   });
+
+//   response.status(200).json({ members });
+// }
